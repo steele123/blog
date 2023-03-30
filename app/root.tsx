8343874f1759073a3f1d5e24f538ea93e87c0d11
Steele Scott <60533996+steele123@users.noreply.github.com>
2023-03-30 17:45:04 -0400

    Update description

--- app/root.tsx
+++ app/root.tsx
@@ -4,14 +4,16 @@ import tailwindCss from './styles/tailwind.css'
 import inter from 'inter-ui/inter.css'
 import globalCss from './styles/globals.css'
 
+const description = "hi, i'm steele, a software engineer. wanna contact me? email me at me@stele.me or on discord steele#7375"
+
 export const meta: MetaFunction = () => ({
     charset: "utf-8",
     title: "steele's website",
     viewport: "width=device-width,initial-scale=1",
     "og:title": "steele's website",
     "og:image": "https://stele.me/steele.jpg",
-    description: "hi, i'm steele. i'm a software engineer wanna contact me? email me at me@stele.me",
-    "og:description": "hi, i'm steele. i'm a software engineer wanna contact me? email me at me@stele.me",
+    description: description,
+    "og:description": description,
     "og:color": "#000000",
 });
 
